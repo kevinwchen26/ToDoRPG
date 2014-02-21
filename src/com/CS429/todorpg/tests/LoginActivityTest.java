9a243c1db5eabf56e8ee8248d1c50d64c67b518a
Kevin <kevinwchen26@gmail.com>
2014-02-21 03:29:13 -0600

    kwchen3 lchen59:setup unit testing

---
+++ src/com/CS429/todorpg/tests/LoginActivityTest.java
@@ -0,0 +1,18 @@
+package com.CS429.todorpg.tests;
+
+import com.CS429.todorpg.LoginActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class LoginActivityTest extends
+		ActivityInstrumentationTestCase2<LoginActivity> {
+
+	public LoginActivityTest() {
+		super(LoginActivity.class);
+	}
+
+	public void testLogin() {
+		assertTrue(true);
+	}
+
+}
