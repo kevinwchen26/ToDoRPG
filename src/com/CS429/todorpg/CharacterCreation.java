7e10c5ad9a19a9e1b2352806f93c383f4597fefd
hlim10 <hlim10@illinois.edu>
2014-02-21 14:51:30 -0600

    hlim10 jcheng26: Changed to ScrollView to prevent the parts of screen from not being able to displayed

--- src/com/CS429/todorpg/CharacterCreation.java
+++ src/com/CS429/todorpg/CharacterCreation.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -30,6 +31,7 @@ public class CharacterCreation extends Activity implements OnClickListener, OnIt
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_character_creation);
 		// Initialize Spinner
