d87804e3d917ad6ee103d388f89650c9c10abde1
rackadone <rackadone@gmail.com>
2014-02-21 07:55:57 -0600

    paulkim6, ssong25: Edit layout

--- src/com/CS429/todorpg/CharacterCreation.java
+++ src/com/CS429/todorpg/CharacterCreation.java
@@ -1,35 +1,42 @@
 package com.CS429.todorpg;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class CharacterCreation extends Activity implements OnClickListener{
+public class CharacterCreation extends Activity implements OnClickListener, OnItemSelectedListener{
 
 	int curidx;
 	Drawable characters[];
 	ImageView img;
+	EditText characterName;
+	Spinner spinner;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_character_creation);
 		// Initialize Spinner
-		Spinner spinner = (Spinner) findViewById(R.id.select_class_spinner);
+		spinner = (Spinner) findViewById(R.id.select_class_spinner);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
 		        R.array.character_classes, android.R.layout.simple_spinner_item);
 		// Specify the layout to use for choices
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
-		
+		spinner.setOnItemSelectedListener(this);
 		// Initialize character array
 		setCharacters();
 		
@@ -37,14 +44,14 @@ public class CharacterCreation extends Activity implements OnClickListener{
 	
 		img = (ImageView)findViewById(R.id.character_image);
 		
-		
-		
-		
+		// Arrows
 		ImageButton left = (ImageButton)findViewById(R.id.leftarrow);
 		ImageButton right = (ImageButton)findViewById(R.id.rightarrow);
 		left.setOnClickListener(this);
 		right.setOnClickListener(this);
 		
+		// Name
+		characterName = (EditText)findViewById(R.id.character_name);
 		 
 	}
 
@@ -85,5 +92,28 @@ public class CharacterCreation extends Activity implements OnClickListener{
 		
 	}
 	
+	String selected;	//temp variable
+	@SuppressLint("NewApi")
+	public void tempMessage(View view) {
+		String name = characterName.getText().toString();
+		if (name == null || name.isEmpty())
+			name = "Empty String";
+		Toast.makeText(getApplicationContext(), name, Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), selected, Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
+			long id) {
+		
+		selected = parent.getItemAtPosition(pos).toString();
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		selected = "Nothing Selected";
+	}
 
 }
