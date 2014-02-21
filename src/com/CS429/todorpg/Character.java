6e77d1b245e880ed9b5a31329069bf8e3cc6159b
Johnny <jcheng26@illinois.edu>
2014-02-20 23:04:58 -0600

    jcheng26 hlim10: Added generic character class, working on junit test cases

--- src/com/CS429/todorpg/Character.java
+++ src/com/CS429/todorpg/Character.java
@@ -3,22 +3,60 @@ package com.CS429.todorpg;
 public class Character {
 	private String Name;
 	
-	
-	private int HP;
-	private int MP;
-	private int Atk;
-	private int PDef;
-	private int MDef;
+	private int LEVEL;
+	private int CON;
+	private int STR;
+	private int DEX;
+	private int INT;
+	private int WIS;
+	private int CHA;
+	private int currentEXP;
+	private int nextLevelEXP;
 	
 	public Character(String Name) {
 		this.Name = Name;
-		this.HP = 100;
-		this.MP = 50;
-		this.Atk = 10;
-		this.PDef = 10;
-		this.MDef = 10;
+		this.CON = 10;
+		this.STR = 10;
+		this.DEX = 10;
+		this.INT = 10;
+		this.WIS = 10;
+		this.CHA = 10;
+		this.currentEXP = 0;
+		this.nextLevelEXP = 100;
 		
-	};
+		
+	}
+	
+	public void Skill_1() {
+		//STUB METHOD
+	}
+	public void Skill_2() {
+		//STUB METHOD
+	}
+	public void Skill_3() {
+		//STUB METHOD
+	}
+	public void Skill_4() {
+		//STUB METHOD
+	}
+	public void levelUP() {
+		this.LEVEL++;
+		int overEXP = this.currentEXP - this.nextLevelEXP;
+		this.currentEXP = overEXP;
+		this.nextLevelEXP =  this.nextLevelEXP * 2;
+		this.LevelStats();
+		
+	}
+	
+	public void LevelStats() {
+		System.out.println("I am generic");
+	}
+	
+	public void checkEXP(){
+		System.out.println("I am generic");
+		if(this.currentEXP >= this.nextLevelEXP)
+			this.levelUP();
+	}
 	
 	
 }
