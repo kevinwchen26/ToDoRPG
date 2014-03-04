42348f5025832020c52940bfadc1f284ae1ac25d
Kevin <kevinwchen26@gmail.com>
2014-03-03 21:21:34 -0600

    added onclick listener for markers

--- src/com/CS429/todorpg/MapActivity.java
+++ src/com/CS429/todorpg/MapActivity.java
@@ -1,34 +1,38 @@
 package com.CS429.todorpg;
 
+import java.util.ArrayList;
+
 import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.model.*;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements OnMarkerClickListener {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+		// Get a handle to the Map Fragment
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(
+				R.id.map)).getMap();
 
-        // Get a handle to the Map Fragment
-        GoogleMap map = ((MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
+		LatLng sydney = new LatLng(-33.867, 151.206);
 
-        LatLng sydney = new LatLng(-33.867, 151.206);
+		map.setMyLocationEnabled(true);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
-    }
+		for (MarkerOptions option : getQuests()) {
+			map.addMarker(option);
+		}
+		map.addMarker(new MarkerOptions().title("Sydney")
+				.snippet("The most populous city in Australia.")
+				.position(sydney));
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,4 +41,21 @@ public class MapActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onMarkerClick(Marker marker) {
+		// pulls up quest info page
+		// TODO link to quest info page
+		return false;
+	}
+
+	public ArrayList<MarkerOptions> getQuests() {
+		ArrayList<MarkerOptions> options = new ArrayList<MarkerOptions>();
+		MarkerOptions option = new MarkerOptions();
+		LatLng position = new LatLng(0, 0);
+		option.snippet("Body");
+		option.title("title");
+		option.position(position);
+		return options;
+
+	}
 }
