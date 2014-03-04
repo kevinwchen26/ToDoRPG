5a709978d43ed9d4e9d6f52f1f7c89f2498747b0
Johnny <jcheng26@illinois.edu>
2014-03-03 22:43:57 -0600

    paulkim6 jcheng26: started form for creating quest

--- src/com/CS429/todorpg/QuestCreation.java
+++ src/com/CS429/todorpg/QuestCreation.java
@@ -2,12 +2,24 @@ package com.CS429.todorpg;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.EditText;
 
 public class QuestCreation extends Activity {
+	
+	EditText title;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.quest_creation);
+		FindViewByID();
+	}
+	
+	private void FindViewByID() {
+		
+	
 	}
 
 }
