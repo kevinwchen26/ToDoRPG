49490638aae24b3371a31a030eba039cfd8e5a98
Kevin <kevinwchen26@gmail.com>
2014-04-01 15:38:22 -0500

    kwchen3 paulkim3: finished character selection spinner

--- ToDoRPG/Android/Tests/ToDoTests/src/com/CS429/todorpg/test/CharacterCreationTests.java
+++ ToDoRPG/Android/Tests/ToDoTests/src/com/CS429/todorpg/test/CharacterCreationTests.java
@@ -32,38 +32,120 @@ public class CharacterCreationTests extends ActivityInstrumentationTestCase2<Cha
 
 	}
 
+	/*
+	 * Tests if activity is setup correctly and the initial items are all on the
+	 * screen
+	 */
 	public void testActivitySetup() {
 		ImageView image = (ImageView) activity.findViewById(R.id.skin_selection);
 		assertNotNull(image);
 		TextView text = (TextView) activity.findViewById(R.id.character_name_txt);
 		assertNotNull(text);
-		assertEquals(text.getText(), activity.getText(R.string.character_name));
+		assertEquals(activity.getText(R.string.character_name), text.getText());
 		EditText namebox = (EditText) activity.findViewById(R.id.character_name);
 		assertNotNull(namebox);
-		assertEquals(namebox.getHint(), activity.getText(R.string.character_name));
+		assertEquals(activity.getText(R.string.character_name), namebox.getHint());
 		ImageButton button = (ImageButton) activity.findViewById(R.id.left_button);
 		assertNotNull(button);
 		button = (ImageButton) activity.findViewById(R.id.right_button);
 		assertNotNull(button);
-		Spinner spinner =(Spinner)activity.findViewById(R.id.character_spinner);
+		Spinner spinner = (Spinner) activity.findViewById(R.id.character_spinner);
 		assertNotNull(spinner);
 	}
 
+	/*
+	 * checks to see if the skin buttons cycles correctly, including out of
+	 * bounds error
+	 */
 	public void testSkinButtons() {
 		ImageView image = (ImageView) activity.findViewById(R.id.skin_selection);
 		assertNotNull(image);
-		solo.clickOnImageButton(1);//tests left button
-		assertEquals(image.getTag(), "dark");
-		solo.clickOnImageButton(0);//test right button
-		assertEquals(image.getTag(),"vampire");
+		solo.clickOnImageButton(1);// tests left button
+		assertEquals("dark", image.getTag());
+		solo.clickOnImageButton(0);// test right button
+		assertEquals("vampire", image.getTag());
 		solo.clickOnImageButton(0);
-		assertEquals(image.getTag(),"green");
+		assertEquals("green", image.getTag());
 		solo.clickOnImageButton(0);
-		assertEquals(image.getTag(),"green");
+		assertEquals("green", image.getTag());
 		solo.clickOnImageButton(1);
-		assertEquals(image.getTag(),"vampire");
+		assertEquals("vampire", image.getTag());
 		solo.clickOnImageButton(1);
-		assertEquals(image.getTag(),"dark");
+		assertEquals("dark", image.getTag());
 	}
 
+	/*
+	 * tests if warrior selection works properly on the spinner
+	 */
+	public void testWarriorSpinner() {
+		Spinner spinner = (Spinner) activity.findViewById(R.id.character_spinner);
+		assertNotNull(spinner);
+		assertEquals(activity.getText(R.string.charter_spinner_prompt), spinner.getPrompt());
+		solo.pressSpinnerItem(0, 1);
+		assertTrue(solo.isSpinnerTextSelected("Warrior"));
+		assertTrue(solo.searchText("Warrior Skill 1"));
+		assertTrue(solo.searchText("Warrior Skill 2"));
+		assertTrue(solo.searchText("Warrior Skill 3"));
+		assertTrue(solo.searchText("Warrior Skill 4"));
+	}
+
+	/*
+	 * tests if assassins selection works properly on the spinner
+	 */
+	public void testAssassinSpinner() {
+		Spinner spinner = (Spinner) activity.findViewById(R.id.character_spinner);
+		assertNotNull(spinner);
+		assertEquals(activity.getText(R.string.charter_spinner_prompt), spinner.getPrompt());
+		solo.pressSpinnerItem(0, 2);
+		assertTrue(solo.isSpinnerTextSelected("Assassin"));
+		assertTrue(solo.searchText("Assassin Skill 1"));
+		assertTrue(solo.searchText("Assassin Skill 2"));
+		assertTrue(solo.searchText("Assassin Skill 3"));
+		assertTrue(solo.searchText("Assassin Skill 4"));
+	}
+
+	/*
+	 * tests if mage selection works properly on spinner
+	 */
+	public void testMageSpinner() {
+		Spinner spinner = (Spinner) activity.findViewById(R.id.character_spinner);
+		assertNotNull(spinner);
+		assertEquals(activity.getText(R.string.charter_spinner_prompt), spinner.getPrompt());
+		solo.pressSpinnerItem(0, 3);
+		assertTrue(solo.isSpinnerTextSelected("Mage"));
+		assertTrue(solo.searchText("Mage Skill 1"));
+		assertTrue(solo.searchText("Mage Skill 2"));
+		assertTrue(solo.searchText("Mage Skill 3"));
+		assertTrue(solo.searchText("Mage Skill 4"));
+	}
+
+	/*
+	 * tests if archer selection works properly on spinner
+	 */
+	public void testArcherSpinner() {
+		Spinner spinner = (Spinner) activity.findViewById(R.id.character_spinner);
+		assertNotNull(spinner);
+		assertEquals(activity.getText(R.string.charter_spinner_prompt), spinner.getPrompt());
+		solo.pressSpinnerItem(0, 4);
+		assertTrue(solo.isSpinnerTextSelected("Archer"));
+		assertTrue(solo.searchText("Archer Skill 1"));
+		assertTrue(solo.searchText("Archer Skill 2"));
+		assertTrue(solo.searchText("Archer Skill 3"));
+		assertTrue(solo.searchText("Archer Skill 4"));
+	}
+
+	/*
+	 * Tests if summoner selection works properly on spinner
+	 */
+	public void testSummonerSpinner() {
+		Spinner spinner = (Spinner) activity.findViewById(R.id.character_spinner);
+		assertNotNull(spinner);
+		assertEquals(activity.getText(R.string.charter_spinner_prompt), spinner.getPrompt());
+		solo.pressSpinnerItem(0, 5);
+		assertTrue(solo.isSpinnerTextSelected("Summoner"));
+		assertTrue(solo.searchText("Summon Skill 1"));
+		assertTrue(solo.searchText("Summon Skill 2"));
+		assertTrue(solo.searchText("Summon Skill 3"));
+		assertTrue(solo.searchText("Summon Skill 4"));
+	}
 }
