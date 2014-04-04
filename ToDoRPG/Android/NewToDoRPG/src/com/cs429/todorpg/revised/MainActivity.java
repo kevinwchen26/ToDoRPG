7f5e44f3f8ab60f4c0976ba70a668a39505bb6a5
Johnny <jcheng26@illinois.edu>
2014-04-03 19:03:46 -0500

    paulkim6 jcheng26: Added rewards activity

--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/MainActivity.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/MainActivity.java
@@ -1,22 +1,40 @@
 package com.cs429.todorpg.revised;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
+	Button rewardsButton;
+	Intent intent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		rewardsButton = (Button) findViewById(R.id.rewards_button);
+		rewardsButton.setOnClickListener(ButtonListener);
+		
 	}
+	
+	Button.OnClickListener ButtonListener = new Button.OnClickListener() {
+		@SuppressLint("NewApi")
+		@Override
+		public void onClick(View view) {
+			switch (view.getId()) {
+			case R.id.rewards_button:
+				intent = new Intent(MainActivity.this, Rewards.class);
+				startActivity(intent);
+				break;
+			
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+			}
+		}
+	};
+	
+	
 
 }
