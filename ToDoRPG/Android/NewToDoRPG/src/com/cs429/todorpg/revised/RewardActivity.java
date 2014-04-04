1afdea3783b44454dde30dcfcb7d651faa13a151
Johnny <jcheng26@illinois.edu>
2014-04-04 17:49:42 -0500

    paulkim6 jcheng26: added purchase button to reward activity.

--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/RewardActivity.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/RewardActivity.java
@@ -4,27 +4,91 @@ import java.util.ArrayList;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class RewardActivity extends Activity {
 
 	TextView rewards_heading;
-	static TextView gold;
-	static Character test_character = new Character();
+	TextView gold;
+	Character test_character = new Character();
 	Button add_reward;
 	EditText add_reward_edit;
-	static ArrayList<Reward> reward_data = new ArrayList<Reward>();
+	ArrayList<Reward> reward_data = new ArrayList<Reward>();
 	ListView reward_list;
+	
+	private class RewardsAdapter extends ArrayAdapter<Reward>{
+
+	    Context context; 
+	    int layoutResourceId;    
+	    ArrayList<Reward> data = null;
+	    
+	    public RewardsAdapter(Context context, int layoutResourceId, ArrayList<Reward> data) {
+	        super(context, layoutResourceId, data);
+	        this.layoutResourceId = layoutResourceId;
+	        this.context = context;
+	        this.data = data;
+	    }
+
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        View row = convertView;
+	        RewardHolder holder = null;
+	        
+	        if(row == null)
+	        {
+	            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+	            row = inflater.inflate(layoutResourceId, parent, false);
+	            
+	            holder = new RewardHolder();
+	            holder.cost = (TextView)row.findViewById(R.id.reward_cost);
+	            holder.info = (TextView)row.findViewById(R.id.reward_title);
+	            holder.button = (Button)row.findViewById(R.id.purchase_reward);
+	            
+	            row.setTag(holder);
+	        }
+	        else
+	        {
+	            holder = (RewardHolder)row.getTag();
+	        }
+	        
+	        Reward reward = data.get(position);
+	        holder.info.setText(reward.getInfo());
+	        holder.cost.setText(Integer.toString(reward.getCost()));
+	        final int cost = reward.getCost();
+	        
+	        holder.button.setOnClickListener(new Button.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					update(cost);
+				}
+	        });
+	        
+	        row.findViewById(R.id.purchase_reward).setTag(position);
+	        
+	        return row;
+	    }
+	    
+	    public class RewardHolder
+	    {
+	        TextView cost;
+	        TextView info;
+	        Button button;
+	    }
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,8 +114,9 @@ public class RewardActivity extends Activity {
 
 	}
 	
-	public static void update(int i) {
+	public void update(int i) {
 		int value = test_character.getGold() - i;
+		test_character.setGold(value);
 	    gold.setText("Gold: " + value );
 
 	}
--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/RewardsAdapter.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/RewardsAdapter.java
@@ -1,64 +1,64 @@
-package com.cs429.todorpg.revised;
-
-import java.util.ArrayList;
-
-import android.app.Activity;
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.TextView;
-
-public class RewardsAdapter extends ArrayAdapter<Reward>{
-
-    Context context; 
-    int layoutResourceId;    
-    ArrayList<Reward> data = null;
-    
-    public RewardsAdapter(Context context, int layoutResourceId, ArrayList<Reward> data) {
-        super(context, layoutResourceId, data);
-        this.layoutResourceId = layoutResourceId;
-        this.context = context;
-        this.data = data;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        RewardHolder holder = null;
-        
-        if(row == null)
-        {
-            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-            row = inflater.inflate(layoutResourceId, parent, false);
-            
-            holder = new RewardHolder();
-            holder.cost = (TextView)row.findViewById(R.id.reward_cost);
-            holder.info = (TextView)row.findViewById(R.id.reward_title);
-            holder.button = (Button)row.findViewById(R.id.purchase_reward);
-            
-            row.setTag(holder);
-        }
-        else
-        {
-            holder = (RewardHolder)row.getTag();
-        }
-        
-        Reward reward = data.get(position);
-        holder.info.setText(reward.getInfo());
-        holder.cost.setText(Integer.toString(reward.getCost()));
-        
-        row.findViewById(R.id.purchase_reward).setTag(position);
-        
-        return row;
-    }
-    
-    static class RewardHolder
-    {
-        TextView cost;
-        TextView info;
-        Button button;
-    }
-}
+//package com.cs429.todorpg.revised;
+//
+//import java.util.ArrayList;
+//
+//import android.app.Activity;
+//import android.content.Context;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//public class RewardsAdapter extends ArrayAdapter<Reward>{
+//
+//    Context context; 
+//    int layoutResourceId;    
+//    ArrayList<Reward> data = null;
+//    
+//    public RewardsAdapter(Context context, int layoutResourceId, ArrayList<Reward> data) {
+//        super(context, layoutResourceId, data);
+//        this.layoutResourceId = layoutResourceId;
+//        this.context = context;
+//        this.data = data;
+//    }
+//
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+//        View row = convertView;
+//        RewardHolder holder = null;
+//        
+//        if(row == null)
+//        {
+//            LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+//            row = inflater.inflate(layoutResourceId, parent, false);
+//            
+//            holder = new RewardHolder();
+//            holder.cost = (TextView)row.findViewById(R.id.reward_cost);
+//            holder.info = (TextView)row.findViewById(R.id.reward_title);
+//            holder.button = (Button)row.findViewById(R.id.purchase_reward);
+//            
+//            row.setTag(holder);
+//        }
+//        else
+//        {
+//            holder = (RewardHolder)row.getTag();
+//        }
+//        
+//        Reward reward = data.get(position);
+//        holder.info.setText(reward.getInfo());
+//        holder.cost.setText(Integer.toString(reward.getCost()));
+//        
+//        row.findViewById(R.id.purchase_reward).setTag(position);
+//        
+//        return row;
+//    }
+//    
+//    static class RewardHolder
+//    {
+//        TextView cost;
+//        TextView info;
+//        Button button;
+//    }
+//}
