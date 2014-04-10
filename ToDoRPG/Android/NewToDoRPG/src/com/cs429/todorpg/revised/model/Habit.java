8a5c67c3f5b4d3b920045c84fe1a534e7f6b4a02
hlim10 <hlim10@illinois.edu>
2014-04-10 07:52:34 -0500

    hlim10 ssong25 : modified Habit, todo, dailiy class

--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/model/Habit.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/model/Habit.java
@@ -1,17 +1,49 @@
 package com.cs429.todorpg.revised.model;
 
-
 public class Habit {
 	private String my_habit;
+	private String extra;
+	private int primary_key;
+
 	public Habit(String my_habit) {
-		this.my_habit = my_habit;
+		this.setHabit(my_habit);
 	}
-	
+
+	public Habit(String my_habit, String extra, int primary_key) {
+		this.setHabit(my_habit);
+		this.setExtra(extra);
+		this.setKey(primary_key);
+	}
+
 	public String getHabit() {
 		return my_habit;
 	}
+
 	public void setHabit(String my_habit) {
 		this.my_habit = my_habit;
 	}
+
+	public String getExtra() {
+		return extra;
+	}
+
+	public void setExtra(String extra) {
+		this.extra = extra;
+	}
+
+	public int getKey() {
+		return primary_key;
+	}
+
+	public void setKey(int primary_key) {
+		this.primary_key = primary_key;
+	}
 	
+	public boolean equals(Object o) {
+		Habit habit = (Habit) o;
+		return (this.primary_key == habit.getKey()
+				&& this.my_habit.equals(habit.getHabit()) && this.extra
+					.equals(habit.getExtra()));
+	}
+
 }
