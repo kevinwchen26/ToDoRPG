09003944501c783d5056909f0913b181b681cd59
ssong25 <ssong25@illinois.edu>
2014-04-14 02:37:53 -0500

    ssong25, hlim10: alarm feature
    alarm feature is added

--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/DailyActivity.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/DailyActivity.java
@@ -2,22 +2,34 @@ package com.cs429.todorpg.revised;
 
 import java.util.ArrayList;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.cs429.todoprg.service.AlarmReceiver;
+import com.cs429.todoprg.service.AlarmService;
 import com.cs429.todorpg.revised.controller.DailyAdapter;
 import com.cs429.todorpg.revised.model.Daily;
 
 
 public class DailyActivity extends BaseActivity {
 	private EditText add_daily_field;
+	private CheckBox alarm_check;
 	private ListView daily_list;
 	private ArrayList<Daily> daily;
 	private DailyAdapter adapter;
+	
+	private SharedPreferences pref;
+	private boolean IsAlarmSet;
+	private final int ALARMCODE = 77;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,13 +40,63 @@ public class DailyActivity extends BaseActivity {
 		// new today_vice().execute();
 		findViewById();
 	}
-
+	
+	@Override
+	public void onResume(){
+		Log.e("[LifeCycle]", "DailyActivity: ++onResume++");
+		super.onResume();
+		pref = this.getSharedPreferences("ToDoRPG", MODE_PRIVATE);
+		IsAlarmSet = pref.getBoolean("alarm", false);
+		init();
+	}
+	
+	@Override
+	public void onPause(){
+		Log.e("[LifeCycle]", "DailyActivity: ++onPause++");
+		super.onPause();
+		SharedPreferences.Editor ed = pref.edit();
+		ed.putBoolean("alarm", IsAlarmSet);
+		ed.commit();
+	}
+	
 	private void findViewById() {
 		add_daily_field = (EditText) findViewById(R.id.add_daily_field);
 		daily_list = (ListView) findViewById(R.id.daily_listview);
 		findViewById(R.id.add_daily_button).setOnClickListener(ButtonHandler);
+		alarm_check = (CheckBox) findViewById(R.id.alarm_checkbox);
+		alarm_check.setOnCheckedChangeListener(CheckListener);
 		
 	}
+	
+	private void init(){
+		if(IsAlarmSet){
+			Log.d("[DAY]", "alarm has already been invoked..??");
+			alarm_check.setChecked(true);
+		}
+	}
+	
+	CheckBox.OnCheckedChangeListener CheckListener = new CheckBox.OnCheckedChangeListener(){
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			
+			Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+			//set alarm
+			if(isChecked){
+				IsAlarmSet = isChecked;
+				Log.d("[DAY]", "Alarm invoked");
+				intent.putExtra("purpose", "invoke");
+			}
+			//cancel alarm
+			else{
+				IsAlarmSet = isChecked;
+				Log.d("[DAY]", "Alarm canceled");
+				intent.putExtra("purpose", "cancel");
+			}
+			intent.putExtra("Alarmkey", ALARMCODE);
+			Log.d("[Alarm]", "key is : " + ALARMCODE);
+			sendBroadcast(intent);
+		}
+	};
 	
 	Button.OnClickListener ButtonHandler = new Button.OnClickListener() {
 		@Override
@@ -73,104 +135,5 @@ public class DailyActivity extends BaseActivity {
 		
 
 	}
-
 }
 
-/*	private void findViewById(){
-		daily_list_view = (ListView)findViewById(R.id.daily_listview);
-		add_button = (Button)findViewById(R.id.add_button);
-		add_button.setOnClickListener(ButtonListener);
-		edit = (EditText)findViewById(R.id.list_edit);
-	}
-	
-	
-	private ArrayList<Daily> setDailyList(){
-		//temporary lists
-		ArrayList<Daily> tmp = new ArrayList<Daily>();
-		Daily tmp1 = new Daily("daily1");
-		tmp1.setImage(R.drawable.temp_images);
-		tmp.add(tmp1);
-		
-		Daily tmp2 = new Daily("daily2");
-		tmp2.setImage(R.drawable.temp_images);
-		tmp.add(tmp2);
-		
-		Daily tmp3 = new Daily("daily3");
-		tmp3.setImage(R.drawable.temp_images);
-		tmp.add(tmp3);
-		////////
-		
-		return tmp;
-	}
-	
-	private void setDailyListView(){
-		adapter = new DailyListAdapter(
-				DailyActivity.this, daily_list);
-		
-		View header = (View)getLayoutInflater().inflate(R.layout.quest_header, null);
-		TextView txt = (TextView)header.findViewById(R.id.txtHeader);
-		txt.setText("DAILY LIST");
-		daily_list_view.addHeaderView(header);
-		daily_list_view.setAdapter(adapter);
-	}	
-	
-	private boolean addListFromEdit(EditText newtitle){
-		String text = newtitle.getText().toString();
-		if(text == null || text.isEmpty())
-			return false;
-		
-		Daily Daily = new Daily(text);
-		Daily.setImage(R.drawable.temp_images);
-		//temporary due date set
-		Daily.setDueDate(Calendar.getInstance().get(Calendar.MONTH),
-			Calendar.getInstance().get(Calendar.DAY_OF_MONTH), 0, 0);
-		daily_list.add(0, Daily);
-		newtitle.setText(null);
-		DataBaseManager.getInstance(getApplicationContext()).insertDAILY(Daily);
-		return true;
-	}
-	
-	OnClickListener ButtonListener = new OnClickListener(){
-		@Override
-		public void onClick(View view) {
-			switch(view.getId()){
-			
-			case R.id.add_button:
-				if(addListFromEdit(edit))
-					adapter.notifyDataSetChanged();
-				else
-					Toast.makeText(DailyActivity.this, "type in title", Toast.LENGTH_SHORT).show();
-				break;				
-			default:
-				break;
-			}
-		}
-	};
-	
-class today_daily extends AsyncTask<String, String, String> {
-		
-		Calendar calendar;
-		int month;
-		int day;
-		protected void onPreExecute() {
-			super.onPreExecute();
-			DataBaseManager.getInstance(DailyActivity.this);
-			calendar = Calendar.getInstance();
-			month = calendar.get(Calendar.MONTH);
-			day = calendar.get(Calendar.DAY_OF_MONTH);
-		}
-
-		@Override
-		protected String doInBackground(String... args) {
-			daily_list = DataBaseManager.getInstance(DailyActivity.this).getDaily(month, day);
-			if(daily_list == null)
-				daily_list = new ArrayList<Daily>();
-			return null;
-		}
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			setDailyListView();	
-		}
-	}
-	
-} */
