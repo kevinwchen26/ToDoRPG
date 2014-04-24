4a5475a134b4a6450a87e6217b3fdfedd3d62072
hlim10 <hlim10@illinois.edu>
2014-04-24 14:39:44 -0500

    hlim10 kchen26 : Modify errors + add Battle Activity

--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/BattleActivity.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/BattleActivity.java
@@ -3,13 +3,11 @@ package com.cs429.todorpg.revised;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-
-public class ShopActivity extends BaseActivity {
-	
+public class BattleActivity  extends BaseActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.shop_activity);
+		setContentView(R.layout.battle_activity);
 	}
 }
--- ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/MainActivity.java
+++ ToDoRPG/Android/NewToDoRPG/src/com/cs429/todorpg/revised/MainActivity.java
@@ -2,6 +2,7 @@ package com.cs429.todorpg.revised;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.cs429.todorpg.revised.utils.SQLiteHelper;
@@ -13,10 +14,12 @@ public class MainActivity extends BaseActivity {
 	Intent intent;
 	SQLiteHelper sql;
 	ToDoCharacter character;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		sql = new SQLiteHelper(this);
 		CreateCharacter();
 		setHeader(R.id.header);
@@ -24,11 +27,10 @@ public class MainActivity extends BaseActivity {
 	}
 
 	private void CreateCharacter() {
+		character = sql.getCharacter();
 		if (character == null) {
 			character = new ToDoCharacter("Hello", 0, 100, 1, 0, 100);
 			sql.addCharacter(character);
-		} else {
-			character = sql.getCharacter();
 		}
 
 	}
